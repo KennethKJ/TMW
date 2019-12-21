64772256706328657cd02e0a0eb54911f5994afe
kennethkraghjensen <kenneth.kragh.jensen@gmail.com>
2019-12-21 13:59:51 -0500

    Plotting a 1D histogram panel for each feature; Log transformations applied;

--- Part 2/KKJ_Contribution/dataExploration.m
+++ Part 2/KKJ_Contribution/dataExploration.m
@@ -15,6 +15,8 @@ T.label = categorical(T.label);
 Vars = T.Properties.VariableNames;
 Vars(end) = [];
 
+numVars = length(Vars);
+
 % Group labels
 groups = {'female','male'}; % group labels
 cols = 'rb'; % group colors
@@ -24,15 +26,23 @@ N = height(T)/2;
 
 % Clear figure
 figure(100); clf; hold on
-ax = gca;
 
 %% Simple 1D inspection
 
-do_1D_histograms =0;
+
+do_1D_histograms =1;
 if do_1D_histograms
+    
+    is_log_idx = [7 8 14 15 17 18];
+%     is_log_idx = [7 8 14 17 18];
+    is_log = zeros(numVars,1);
+    is_log(is_log_idx) = 1;
+    
     % Loop through all variable and plot malevs female histograms
     for i = 1 : length(Vars)
         
+        % Prep figure subplot
+        ax = subplot(4,5,i);
         % Clear axis
         cla; hold on
         
@@ -44,43 +54,77 @@ if do_1D_histograms
             % Get data from table
             data(:,j) = T.(Vars{i})(T.label == groups{j});
             
-            % plotting histograms
-            histogram(data(:,j),50, 'FaceColor', cols(j), 'FaceAlpha', 0.5)
             
+            if i == 15
+                
+                sum(data(:,1) == 0)
+                % Flip data
+                data(:,j) = 1./data(:,j);  
+                
+            end
+            
+            
+            % Apply transform if necessary
+            if is_log(i)
+                
+                idx_zero = data(:,j) == 0;
+                if ~isempty(idx_zero) && sum(idx_zero) > 0
+                    data(idx_zero,j) = eps; % remove zeros
+                end
+                data(:,j) = log2(data(:,j));
+                ax.GridColor = 'r';
+                ax.XColor = 'r';
+                ax.YColor = 'r';
+                
+            else
+%                 title(i)
+            end
+            
+            
+        end
+        
+
+        
+        bins = linspace(min(data(:)), max(data(:)), 50);  % Calculate bins for histograms
+        for j = 1 : 2
+            % plotting histograms
+            histogram(data(:,j),bins, 'FaceColor', cols(j), 'FaceAlpha', 0.5, 'Normalization','probability')
         end
         
         
         %% Statistical significance
         
-        % T-test
-        [~, p_t] = ttest2(data(:,1), data(:,2),'Vartype','unequal');
-        % Mann-Whitney U test
-        p_mw = ranksum(data(:,1),data(:,2));
-        % Sensitivity
-        d_prime = abs(mean(data(:,1)) - mean(data(:,2)))/sqrt(0.5*(std(data(:,1)) + std(data(:,2))));
+%         % T-test
+%         [~, p_t] = ttest2(data(:,1), data(:,2),'Vartype','unequal');
+%         % Mann-Whitney U test
+%         p_mw = ranksum(data(:,1),data(:,2));
+%         % Sensitivity
+%         d_prime = abs(mean(data(:,1)) - mean(data(:,2)))/sqrt(0.5*(std(data(:,1)) + std(data(:,2))));
         
         
-        % plotting mean values
-        for j = 1 : length(groups)
-            plot([mean(data(:,j)) mean(data(:,j))],[0 ax.YLim(2)],cols(j), 'LineWidth',3)
-            plot([median(data(:,j)) median(data(:,j))],[0 ax.YLim(2)],[cols(j) '--'], 'LineWidth',3)
-        end
+%         % plotting mean values
+%         for j = 1 : length(groups)
+%             plot([mean(data(:,j)) mean(data(:,j))],[0 ax.YLim(2)],cols(j), 'LineWidth',3)
+%             plot([median(data(:,j)) median(data(:,j))],[0 ax.YLim(2)],[cols(j) '--'], 'LineWidth',3)
+%         end
         
         
         % Update figure labels etc
-        legend(groups)
+%         legend(groups)
         xlabel(Vars{i})
-        ylabel('Frequency')
+        ylabel('Probability')
+        grid on; box on
+        ylim([0 0.2])
         
-        % Print test reults onto figure
-        x = ax.XLim(1) + 0.1*(ax.XLim(2)-ax.XLim(1));
-        y = ax.YLim(1) + 0.9*(ax.YLim(2)-ax.YLim(1));
-        text(x, y,['T-test p-value = ' num2str(p_t)],'FontSize',16)
-        y = y - 0.05*y;
-        text(x, y,['d prime = ' num2str(d_prime)],'FontSize',16)
-        y = y - 0.05*y;
-        text(x, y,['MW U p-value = ' num2str(p_mw)],'FontSize',16)
-        
+%         % Print test reults onto figure
+%         x = ax.XLim(1) + 0.1*(ax.XLim(2)-ax.XLim(1));
+%         y = ax.YLim(1) + 0.9*(ax.YLim(2)-ax.YLim(1));
+%         text(x, y,['T-test p-value = ' num2str(p_t)],'FontSize',16)
+%         y = y - 0.05*y;
+%         text(x, y,['d prime = ' num2str(d_prime)],'FontSize',16)
+%         y = y - 0.05*y;
+%         text(x, y,['MW U p-value = ' num2str(p_mw)],'FontSize',16)
+%         
         
     end
 end
@@ -91,6 +135,9 @@ figure(100); clf; hold on
 
 do_scatter = 1;
 if do_scatter
+    
+    Vars = sensitivitySort(T);
+    
     % Loop through all variable and plot malevs female histograms
     for i = 1 : length(Vars)-1
         
@@ -132,5 +179,42 @@ end
 
 disp('DONE!')
 
+sorted = sensitivitySort(T);
 
 
+
+function sorted = sensitivitySort(T)
+
+
+groups = unique(T.label);
+
+% Get all the variable names and remove the label
+Vars = T.Properties.VariableNames;
+Vars(end) = [];
+
+numVars = length(Vars);
+
+sensitivityMatrix = nan(numVars, 1);
+
+for i = 1 : numVars
+    
+        
+        data = nan(height(T)/2,2);
+
+        for k = 1 : length(groups) % female and male
+            
+            % Get data from table
+            data(:, k) = T.(Vars{i})(T.label == groups(k));
+            
+       
+            
+        end
+        % Sensitivity
+        d_prime(i) = abs(mean(data(:,1)) - mean(data(:,2)))/sqrt(0.5*(std(data(:,1)) + std(data(:,2))));
+        
+end
+
+[~, idx] = sort(d_prime,'descend');
+sorted = Vars(idx);
+
+end
