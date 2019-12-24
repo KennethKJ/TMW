f47a318ca46dda5dec73c0e5191182c85b01a238
kennethkraghjensen <kenneth.kragh.jensen@gmail.com>
2019-12-24 12:06:07 -0500

    Added option of feature crossings Fixed bug in selected features (features now selected by name and not index) Fixed bug in x data selection (non-consequencial)

--- Part 2/KKJ_Contribution/Model/logistic_regr.py
+++ Part 2/KKJ_Contribution/Model/logistic_regr.py
@@ -14,17 +14,38 @@ def get_run_logdir(root_dir_fn):
     os.chdir(full_path)
     return full_path
 
-selected_features = [0, 5, 8]
+selected_features = ['meanfun', 'IQR', 'sp.ent']
 
 # Import data
 data_frame = pd.read_csv("voices.csv")
 
-# Extract features
+# Extract selected features
+df_x = data_frame[selected_features]
+data_x = df_x.values.astype('float32')
 
-data_x = data_frame.values[:, selected_features].astype('float32')
+do_feature_crossings = True
+
+if do_feature_crossings:
+    num_features = len(selected_features)
+    notDone = True
+    i1 = 0
+    i2 = 0
+    while i1 < num_features-1:
+
+        i2 += 1
+
+        data_x = np.concatenate((data_x, np.expand_dims(data_x[:, i1] * data_x[:, i2], axis=1)), axis=1)
+
+        if i2 == num_features-1:
+            i1 += 1
+            i2 = i1
+
+
+# Get or update the number of features
+num_features = data_x.shape[1]
 
 # Normalize
-for i in range(len(selected_features)):
+for i in range(num_features):
     data_x[:, i] = (data_x[:, i]-np.mean(data_x[:, i]))/np.std(data_x[:, i])
 
 
@@ -45,22 +66,22 @@ proportion_test = 1 - proportion_train - proportion_eval
 
 # Extract train, eval, and test datasets
 idx_train = idx[0: int(proportion_train*num_data)]
-X_train = data_x[idx_train, 0:13]
+X_train = data_x[idx_train, :]
 Y_train = data_y[idx_train]
 
 idx_eval = idx[int(proportion_train*num_data): len(idx_train) + int(proportion_eval*num_data)]
-X_eval = data_x[idx_eval, 0:13]
+X_eval = data_x[idx_eval, :]
 Y_eval = data_y[idx_eval]
 
 idx_test = idx[len(idx_train) + len(idx_eval):]
-X_test = data_x[idx_test, 0:13]
+X_test = data_x[idx_test, :]
 Y_test = data_y[idx_test]
 
 # Define model
 num_units = 1
-dim_input = len(selected_features)
+dim_input = num_features
 learning_rate = 0.001
-num_epochs = 100
+num_epochs = 70
 batch_size = 32
 root_dir = 'C:\\Users\\alert\\Google Drive\\TMW Working dir\\Keras\\'
 
@@ -95,5 +116,19 @@ history = model.fit(X_train, Y_train,
 # Save model at end
 runID = "Model_NU" + str(num_units) + "_LR=" + "_BZ=" + str(batch_size) + "_NE=" + str(num_epochs) + "_DI=" + str(dim_input)
 model.save(run_logdir + "\\" + runID + '.h5')
+
+doTest = True
+if doTest:
+    classes = model.predict(X_test, batch_size=128)
+    classes = classes > 0.5
+    classes = np.squeeze(classes)
+
+    truth = Y_test > 0.5
+
+    reality = classes == truth
+    hit = np.round(np.sum(reality)/len(truth)*100)
+
+    print('Test set accuracy = ' + str(hit))
+
 
 print("Done!")
